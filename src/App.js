17c1a2c4b1b67eca97cbceefb207e9c42853b210
gooviuy <gooviuy@outlook.com>
2021-11-11 16:38:42 -0300

    api req added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import "./App.css";
 import Home from "./componets/Home";
 import Search from "./componets/Search";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  NavLink,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [movie, setMovie] = useState(null);
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route path="/search" element={<Search movie={setMovie} />} />
       </Routes>
     </Router>
   );
--- src/componets/Search.jsx
+++ src/componets/Search.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { NavLink } from "react-router-dom";
 
-function Search() {
+function Search({ setMovie }) {
+  const [nombrePelicula, setNombrePelicula] = useState("");
+
+  const onChangeHandler = (e) => {
+    setNombrePelicula(e.target.value);
+    console.log(nombrePelicula);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const result = await axios.get(
+      `https://private.omdbapi.com/?plot=full&apikey=bef9c583&t=${nombrePelicula}`
+    );
+
+    setMovie(result.data);
+  };
+
   return (
     <div>
       <ul>
@@ -9,7 +27,15 @@ function Search() {
           Home
         </NavLink>
       </ul>
-      <div>Lets Search for a Movie</div>
+      <div id="search" className="box">
+        <h2>Pelicula</h2>
+        <form onSubmit={submitHandler}>
+          <input value={nombrePelicula} onChange={onChangeHandler} />
+          <button className="btn btn-success" types="submit">
+            Buscar Pelicula
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
